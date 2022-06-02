93a748d3c0da5d27669346b6cac872c28a4cc5fb
RantingLemming <obake.lemming@gmail.com>
2022-06-01 20:16:57 -0700

    Added some responsiveness to header/navigation, including some svg animation.  Created some home page content with a welcome message and other text.  Fun lemming homage.  Should do more with it.

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -5,8 +5,56 @@ import "./HomePage.css";
 export const HomePage = () => {
 	return (
 		<main className='home-page'>
-			<h1>Welcome to the Home page</h1>
-			<p>Home page content goes here :)</p>
+			<section>
+				<div className='welcome-message'>
+					<div className='letter'>W</div>
+					<div className='letter'>E</div>
+					<div className='letter'>L</div>
+					<div className='letter'>C</div>
+					<div
+						className='letter'
+						style={{
+							transform: "translate(4rem,-2rem) rotate(60deg)",
+						}}
+					>
+						O
+					</div>
+					<div
+						className='letter'
+						style={{
+							transform: "translate(6rem,2rem) rotate(45deg)",
+						}}
+					>
+						M
+					</div>
+					<div
+						className='letter'
+						style={{
+							transform: "translate(6rem,12rem) rotate(60deg)",
+						}}
+					>
+						E
+					</div>
+				</div>
+				<div>
+					<p>You gorgeous lemming.</p>
+					<br />
+					<p>This is my website.</p>
+					<p>It's a bit weird.</p>
+					<br />
+					<p>Intentionally, often.</p>
+					<p>Unintentionally, more often.</p>
+				</div>
+			</section>
+			<section>
+				<p>This website can be summarized as such:</p>
+				<ul>
+					<li>Home Page (YOU ARE HERE)</li>
+					<li>Levels</li>
+					<li>Journal</li>
+					<li>Prototypes</li>
+				</ul>
+			</section>
 		</main>
 	);
 };
